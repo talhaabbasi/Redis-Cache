1b6000659d7c0eeda439b2cbd70a2c05bfb29bd5
Talha Abbasi <talhaabbasi1997@gmail.com>
2020-07-07 11:36:09 +0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express ");
+const fetch = require("node-fetch");
+const redis = require("redis");
+
+const PORT = process.env.PORT || 5000;
+const REDIS_PORT = process.env.PORT || 6379;
+
+const client = redis.createClient(REDIS_PORT);
+
+const app = express();
+
+app.listen(5000, () => {
+  console.log("Listening on port 5000");
+});
